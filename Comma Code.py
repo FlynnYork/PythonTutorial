bb229725546b1a29a8a9a306456733feed1d62dd
FlynnYork <wannamark@gmail.com>
2018-01-05 14:53:23 +0800

    Add files via upload
    Practice Projects
    
    For practice, write programs to do the following tasks.
    
    Comma Code
    
    Say you have a list value like this:
    
    
    spam = ['apples', 'bananas', 'tofu', 'cats']
    Write a function that takes a list value as an argument and returns a string with all the items separated by a comma and a space, with and inserted before the last item. For example, passing the previous spam list to the function would return 'apples, bananas, tofu, and cats'. But your function should be able to work with any list value passed to it.

---
+++ Comma Code.py
@@ -0,0 +1,14 @@
+def fun(spam):
+    value2=''
+    for value in spam:
+        if value !=spam[-1]:
+            value = value+', '
+            print value
+            value2 = value2 + value
+            print value2+'and '+spam[-1]
+    return value2+'and '+spam[-1]
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats','cat', 'bat', 'rat', 'elephant','A', 'B']
+fun(spam)
+print fun(spam)
